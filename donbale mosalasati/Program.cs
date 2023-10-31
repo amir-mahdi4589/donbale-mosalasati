5695a6ec6e4ae4cf27c59a356f9da37f6afbd5fe
ASUS <ASUS@DESKTOP-UEHIM0J>
2023-10-31 21:36:10 +0330

    Add project files.

---
+++ donbale mosalasati/Program.cs
@@ -0,0 +1,22 @@
+﻿//tamrin shomare 6 --donbale mosalati--  amir mahdi hosein njad
+do
+{
+    Console.Write("adad N ra vared konid = ");
+
+    int N =Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("adad M ra vared konid = ");
+
+    int M =Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("donbale mosalasaty bein N M");
+
+    for (int i = N; i <= M; i++)
+    {
+        long S = i * (i + 1) / 2;
+        Console.WriteLine(S);
+    }
+
+    Console.WriteLine("mikhay dobare anzamesh bedi ?y or n");
+} while (Console.ReadLine() == "y");
+Console.WriteLine("----payan----");
